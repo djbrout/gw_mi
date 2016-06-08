6bc197213c22d11cdead7e8cb6ce7ee71b6e255e
Dillon Macbook Air <dbrout@physics.upenn.edu>
2016-06-08 18:55:18 -0400

    can deal with missing parameter file now...

--- recycler_config.py
+++ recycler_config.py
@@ -1,17 +1,17 @@
-trigger_path  = '/data/des41.a/data/desgw/maininjector/test-triggers'
+trigger_path  = '/data/des41.a/data/desgw/maininjector/real-triggers'
 exposure_length = 180. #sec
 hours_available = 3.  # how many hours are we going to observe on night 1
 
 ####Only used for testing####
 test_mjd = 55458.4629
-trigger_id = "M186994"
+trigger_id = "G211117"
 wrap_all_triggers = False
 force_mjd = False
 #############################
 
 #############################
 import os
-os.environ['DESGW_DIR'] = "/data/des41.a/data/desgw/osgsetup/eeups/fnaleups/Linux64/gw/v2.1"
-os.environ['GW_DIR'] = "/data/des41.a/data/desgw/osgsetup/eeups/fnaleups/Linux64/gw/v2.1"
+os.environ['DESGW_DIR'] = "/data/des41.a/data/desgw/osgsetup/eeups/fnaleups/Linux64/gw/v2.2"
+os.environ['GW_DIR'] = "/data/des41.a/data/desgw/osgsetup/eeups/fnaleups/Linux64/gw/v2.2"
 os.environ['DESGW_DATA_DIR'] = os.path.join(os.environ['DESGW_DIR'],"data/")
 #############################
