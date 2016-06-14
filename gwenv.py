0c12dfeb8e0897d7aeef9f73f3803fe326c1ab75
djbrout <djbrout@gmail.com>
2016-06-14 14:13:10 -0400

    Delete gwenv.py

--- gwenv.py
+++
@@ -1,4 +0,0 @@
-import os
-os.environ['DESGW_DIR'] = "/data/des41.a/data/desgw/osgsetup/eeups/fnaleups/Linux64/gw/v2.3"
-os.environ['GW_DIR'] = "/data/des41.a/data/desgw/osgsetup/eeups/fnaleups/Linux64/gw/v2.3"
-os.environ['DESGW_DATA_DIR'] = os.path.join(os.environ['DESGW_DIR'],"data/")
