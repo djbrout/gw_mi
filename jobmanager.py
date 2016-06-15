b7803343f2696f5bef622f1bc3f9d20a5dd5fc6d
Dillon Brout <djbrout@gmail.com>
2016-06-15 12:45:33 -0400

    getting nearby images

--- jobmanager.py
+++ jobmanager.py
@@ -88,9 +88,12 @@ class eventmanager:
         allexposures = dilltools.read('./processing/exposures.list',1, 2, delim=' ')
         print allexposures['EXPTIME'][0]
 
-        EXPTIME =np.array(map(float, map(lambda x: x if not x in ['plate','EXPTIME'] else '-999', allexposures['EXPTIME'])))
-        TELRA =np.array(map(float, map(lambda x: x if x != 'plate' else '-999', allexposures['TELRA'])))
-        TELDEC =np.array(map(float, map(lambda x: x if x != 'plate' else '-999', allexposures['TELDEC'])))
+        EXPTIME =np.array(map(float, map(lambda x: x if not x in ['plate','EXPTIME'] else '-999',
+                                         allexposures['EXPTIME'])))
+        TELRA =np.array(map(float, map(lambda x: x if not x in ['plate','RADEG'] else '-999',
+                                         allexposures['TELRA'])))
+        TELDEC =np.array(map(float, map(lambda x: x if x != 'plate' else '-999',
+                                         allexposures['TELDEC'])))
 
         ww = EXPTIME >= jobmanager_config.min_template_exptime
         exposedRAS = TELRA[ww]
