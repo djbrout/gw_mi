780df6eba784acaf40991a65e9dee244e11a88d2
Dillon Brout <djbrout@gmail.com>
2016-06-15 12:54:53 -0400

    getting nearby images

--- jobmanager.py
+++ jobmanager.py
@@ -104,13 +104,15 @@ class eventmanager:
         exposedDECS = TELDEC[ww]
         exposedNUMS = EXPNUM[ww]
 
+        print min(exposedRAS),max(exposedRAS)
+        print min(ras),max(ras)
         print 'exposedRAS',exposedRAS,exposedRAS.shape
 
         submitexpnums = []
 
         for ra,dec in zip(ras,decs):
             dist = np.array(np.sqrt((ra-exposedRAS)**2 + (dec-exposedDECS)**2))
-            print dist
+            #print dist
             nearby = dist < jobmanager_config.SE_radius
             submitexpnums.extend(exposedNUMS[nearby])
 
