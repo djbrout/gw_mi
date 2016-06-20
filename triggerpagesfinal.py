5de8978179d81c567058472b165850a30070c582
Dillon Brout <djbrout@gmail.com>
2016-06-20 11:32:35 -0400

    home

--- triggerpagesfinal.py
+++ triggerpagesfinal.py
@@ -616,20 +616,20 @@ def make_index_page(webpage_dir, real_or_sim=None):
                         <meta name="viewport" content="width=device-width, initial-scale=1.0"> \
                         <meta name="keywords" content="pinegrow, blocks, bootstrap" />\
                         <meta name="description" content="My new website" />\
-                        <link rel="shortcut icon" href="ico/favicon.png"> \
+                        <link rel="shortcut icon" href="assets/ico/favicon.png"> \
                         <!-- Core CSS -->\
                         <link href="bootstrap/css/bootstrap.min.css" rel="stylesheet"> \
-                        <link href="css/font-awesome.min.css" rel="stylesheet">\
+                        <link href="assets/css/font-awesome.min.css" rel="stylesheet">\
                         <link href="http://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,400,300,600,700" rel="stylesheet">\
                         <link href="http://fonts.googleapis.com/css?family=Lato:300,400,700,300italic,400italic,700italic" rel="stylesheet">\
                         <!-- Style Library -->\
-                        <link href="css/style-library-1.css" rel="stylesheet">\
-                        <link href="css/plugins.css" rel="stylesheet">\
-                        <link href="css/blocks.css" rel="stylesheet">\
-                        <link href="css/custom.css" rel="stylesheet">\
+                        <link href="assets/css/style-library-1.css" rel="stylesheet">\
+                        <link href="assets/css/plugins.css" rel="stylesheet">\
+                        <link href="assets/css/blocks.css" rel="stylesheet">\
+                        <link href="assets/css/custom.css" rel="stylesheet">\
                         <!--[if lt IE 9]>\
-                      <script src="js/html5shiv.js"></script>\
-                      <script src="js/respond.min.js"></script>\
+                      <script src="assets/js/html5shiv.js"></script>\
+                      <script src="assets/js/respond.min.js"></script>\
                     <![endif]-->\
                         <script type="text/javascript">\
                     function ChangeColor(tableRow, highLight)\
@@ -661,11 +661,11 @@ def make_index_page(webpage_dir, real_or_sim=None):
                         </section>'
 
     html += indextable
-    html += '<script type="text/javascript" src="js/jquery-1.11.1.min.js"></script>'
-    html += '<script type="text/javascript" src="js/bootstrap.min.js"></script>'
-    html += '<script type="text/javascript" src="js/plugins.js"></script>'
+    html += '<script type="text/javascript" src="assets/js/jquery-1.11.1.min.js"></script>'
+    html += '<script type="text/javascript" src="assets/js/bootstrap.min.js"></script>'
+    html += '<script type="text/javascript" src="assets/js/plugins.js"></script>'
     html += '<script src="https://maps.google.com/maps/api/js?sensor=true"></script>'
-    html += '<script type="text/javascript" src="js/bskit-scripts.js"></script>\
+    html += '<script type="text/javascript" src="assets/js/bskit-scripts.js"></script>\
                     </body></html>'
 
     if real_or_sim == 'real':
