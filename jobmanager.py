cc2c839fdbd9e744b5602843d4abbb8d8f2f906b
Dillon Brout <djbrout@gmail.com>
2016-07-13 16:59:11 -0400

    popen

--- jobmanager.py
+++ jobmanager.py
@@ -345,9 +345,9 @@ class eventmanager:
         args = ['ssh -t desgw@des41.fnal.gov "source '+ os.path.join(gwpostdir, 'diffimg_setup.sh"')]
         print args
 
-        p = subprocess.Popen(args,stdout=PIPE, stderr=PIPE,shell=True)
-        print p.communicate()
-
+        #p = subprocess.Popen(args,stdout=PIPE, stderr=PIPE,shell=True)
+        #print p.communicate()
+        p = subprocess.Popen(args,stdin=None, stdout=None, stderr=None, close_fds=True,shell=True)
         return
 
 
