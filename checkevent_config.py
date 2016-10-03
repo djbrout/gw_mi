dfe499d588695e967b5a0b1cacd45fa4e03addb5
Dillon Brout <djbrout@gmail.com>
2016-10-03 08:19:58 -0400

    running checkevent

--- checkevent_config.py
+++ checkevent_config.py
@@ -1,4 +1,4 @@
 mode = 'observation' # switch to 'observation' for real data!!
 trigger_outpath = '/data/des41.a/data/desgw/gw_mi/real-triggers'
-sendEveryoneEmails = False
+sendEveryoneEmails = True
 sendtexts = False
